d3f21d10caad2312afa91c15097f5d6a01feef47
Ian Douglas Scott <ian@iandouglasscott.com>
2019-07-18 14:21:08 -0700

    Some style changes

--- wasm.js
+++ wasm.js
@@ -1,10 +1,13 @@
+// This is not necessarily a complete implementation of a dynamic loader, nor
+// is it well tested.
+
 const fs = require('fs');
 
 function read_varuint32(array, idx) {
-    var value = 0;
-    var count = 0;
+    let value = 0;
+    let count = 0;
     while (count < 5) {
-        var b = array[idx + count];
+        let b = array[idx + count];
         value |= (b & 0x7f) << (7 * count);
         count++;
         if ((b & 0x80) == 0) {
@@ -16,53 +19,59 @@ function read_varuint32(array, idx) {
 
 dynamic_libraries = {};
 
-async function load_dynamic_wasm(path, env) {
+async function load_wasm_module(path, env) {
     if (dynamic_libraries[path] !== undefined) {
         return dynamic_libaries[path];
     }
 
-    var data = fs.readFileSync(path);
-    var module = await WebAssembly.compile(data);
+    let data = fs.readFileSync(path);
+    let module = await WebAssembly.compile(data);
 
-    var dylink = WebAssembly.Module.customSections(module, "dylink");
-    dylink_array = new Uint8Array(dylink[0]);
+    let dylink = WebAssembly.Module.customSections(module, "dylink");
+    let dylink_array = new Uint8Array(dylink[0]);
 
-    var idx = 0;
-    var [memorysize, idx] = read_varuint32(dylink_array, idx);
-    var [memoryalignment, idx] = read_varuint32(dylink_array, idx);
-    var [tablesize, idx] = read_varuint32(dylink_array, idx);
-    var [tablealignment, idx] = read_varuint32(dylink_array, idx);
-    var [needed_dynlibs_count, idx] = read_varuint32(dylink_array, idx);
+    let idx = 0;
+    let memorysize, memoryalignment, tablesize, tablealignment,
+        needed_dynlibs_count;
+    [memorysize, idx] = read_varuint32(dylink_array, idx);
+    [memoryalignment, idx] = read_varuint32(dylink_array, idx);
+    [tablesize, idx] = read_varuint32(dylink_array, idx);
+    [tablealignment, idx] = read_varuint32(dylink_array, idx);
+    [needed_dynlibs_count, idx] = read_varuint32(dylink_array, idx);
 
     importObject = {"env": {}};
     Object.assign(importObject.env, env);
 
-    var utf8decoder = new TextDecoder(); 
+    let utf8decoder = new TextDecoder(); 
     for (var i = 0; i < needed_dynlibs_count; i++) {
-        var [length, idx] = read_varuint32(dylink_array, idx);
+        let length;
+        [length, idx] = read_varuint32(dylink_array, idx);
         let path = utf8decoder.decode(dylink[0].slice(idx, idx + length));
-        var library = await load_dynamic_wasm(path, env);
+        let library = await load_wasm_module(path, env);
         Object.assign(importObject.env, library.exports);
         idx += length;
     }
 
 
-    var instance = await WebAssembly.instantiate(module, importObject);
+    let instance = await WebAssembly.instantiate(module, importObject);
     dynamic_libraries[path] = instance;
     return instance;
 }
 
-var memory = new WebAssembly.Memory({'initial': 1024});
-__indirect_function_table = new WebAssembly.Table({element: "anyfunc", initial: 0});
-// TODO determine sensible value for stack pointer (look at what lld does)
-__stack_pointer = new WebAssembly.Global({value: "i32", mutable: true}, 1024);
-env = {
-    "memory": memory,
-    "__indirect_function_table": __indirect_function_table,
-    "__stack_pointer": __stack_pointer,
-    "__memory_base": 0,
-    "__table_base": 0,
-    "print_int": console.log
+function load_wasm(path) {
+    let memory = new WebAssembly.Memory({initial: 1024});
+    let  __indirect_function_table = new WebAssembly.Table({element: "anyfunc", initial: 0});
+    // TODO determine sensible value for stack pointer (look at what lld does)
+    let __stack_pointer = new WebAssembly.Global({value: "i32", mutable: true}, 1024);
+    let env = {
+        memory: memory,
+        __indirect_function_table: __indirect_function_table,
+        __stack_pointer: __stack_pointer,
+        __memory_base: 0,
+        __table_base: 0,
+        print_int: console.log
+    };
+    return load_wasm_module(path, env);
 }
 
-load_dynamic_wasm("bin.wasm", env).then(instance => instance.exports.main());
+load_wasm("bin.wasm").then(instance => instance.exports.main());
